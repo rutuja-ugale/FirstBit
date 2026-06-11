70264a0b59ef0ce54a79dccf5ef6cf79eacb3569
rutujaa121 <rutujaaugale@gmail.com>
2026-06-11 10:18:49 +0530

    add if_else_ladder_demo.py

--- Core Python/Demos/Branching/random_demo_1.py
+++ Core Python/Demos/Branching/random_demo_1.py
@@ -1,2 +1,7 @@
-username = "rutuja"
-userpass = "1234"
+num = int(input('Enter a number: '))
+if(num == 0):
+    print('Number is neutral')
+elif(num > 0):
+    print('Number is positive')
+else:
+    print('Number is negative')
